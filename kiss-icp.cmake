d8af5e8d02530591e60a71428e796b30c6febf48
toriz <atoriz98@comunidad.unam.mx>
2024-05-27 16:05:45 +0200

    catkin_make compiles now. Added KISS-ICP as thirdparty library.

--- kiss-icp.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(
-  kiss-icp
-  GIT_REPOSITORY https://github.com/PRBonn/kiss-icp.git
-  GIT_TAG v0.2.10
-  SOURCE_SUBDIR cpp/kiss_icp
-)
-
-FetchContent_MakeAvailable(kiss-icp)
